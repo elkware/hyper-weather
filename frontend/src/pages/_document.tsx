46a37efc0e67be5a3adc822004d42cab42e2eb89
Jozef Jung <jung.jozsef.m@gmail.com>
2023-04-20 15:14:16 +0200

    Dark mode always

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" data-theme="dark">
       <Head>
       <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,300,0,0" />
       </Head>
